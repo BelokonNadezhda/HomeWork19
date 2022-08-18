70a9e69d83be88b5b973ea6210eb868af9c92cec
BelokonNadezhda <nadegdabel@mail.ru>
2022-08-18 10:40:45 +0300

    тест

--- app/src/main/java/com/example/myapplication19/MainActivity.kt
+++ app/src/main/java/com/example/myapplication19/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.myapplication19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.myapplication19.databinding.ActivityMainBinding
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        //Инициализируем объект
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+
+        binding.buttonMenu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonFav.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonWatchLater.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSelections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSettings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
